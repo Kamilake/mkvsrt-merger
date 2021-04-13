9eb9d8d90df7d76bbd35c4521cd77d80bee7a207
Kamilake <exjang0@gmail.com>
2021-04-13 16:47:42 +0900

    Add files via upload

---
+++ conv.bat
@@ -0,0 +1,107 @@
+@REM @echo off
+echo [This file encoding = EUC-KR]
+echo ==MKV/MP4 SRT 병합 스크립트==
+echo ==Kamilake 제작==
+setlocal
+
+
+IF "%~1" == "" GOTO NOFILE_HELP
+IF "%~2" == "" GOTO ONEFILE
+IF "%~3" == "" GOTO TWOFILE
+IF "%~4" == "" GOTO THREEFILE
+GOTO NOFILE_HELP
+:NOFILE_HELP
+echo ====도움말====
+echo 사용법: conv ^<파일 또는 폴더^> [파일] [출력] [/R]
+echo.
+echo         conv ^<비디오^> ^<자막^> [^<^출력할 파일명^>.^<확장자^>]
+echo         또는
+echo         conv ^<폴더^> [/R]
+echo         또는
+echo         conv ^<비디오^>
+echo         (^* 비디오만 선택한다면 자막은 같은 이름을 가지고 .srt .ko.srt .kor.srt .ko.kor.srt .smi.srt .smi.ko.srt .smi.kor.srt .smi.ko.kor.srt 확장자를 가지고 있어야 합니다.)
+echo.
+GOTO END
+
+
+============================================
+============================================
+============================================
+============================================
+
+:ONEFILE
+IF EXIST %1\* GOTO ONEFILE_ONEDIR
+echo 파일 1개 선택됨
+ IF EXIST "%~p1%~n1.srt" (ffmpeg -i "%~1" -f srt -i "%~p1%~n1.srt" -map 0:0 -map 0:1 -map 1:0 -c:v copy -c:a copy -c:s srt -metadata:s:s:0 language=kor "%~p1%~n1_subs%~x1")
+ IF EXIST "%~p1%~n1.ko.srt" (ffmpeg -i "%~1" -f srt -i "%~p1%~n1.kor.srt" -map 0:0 -map 0:1 -map 1:0 -c:v copy -c:a copy -c:s srt -metadata:s:s:0 language=kor "%~p1%~n1_subs%~x1")
+ IF EXIST "%~p1%~n1.kor.srt" (ffmpeg -i "%~1" -f srt -i "%~p1%~n1.kor.srt" -map 0:0 -map 0:1 -map 1:0 -c:v copy -c:a copy -c:s srt -metadata:s:s:0 language=kor "%~p1%~n1_subs%~x1")
+ IF EXIST "%~p1%~n1.ko.kor.srt" (ffmpeg -i "%~1" -f srt -i "%~p1%~n1.ko.kor.srt" -map 0:0 -map 0:1 -map 1:0 -c:v copy -c:a copy -c:s srt -metadata:s:s:0 language=kor "%~p1%~n1_subs%~x1")
+
+GOTO END
+:ONEFILE_ONEDIR
+echo 폴더 1개 선택됨
+@REM 출력파일명 "%%~pX%%~nX_subs%%~xX"
+@REM not recursive walk를 원한다면 /r %1을 제거하고 cd %1로 폴더에 들어가야 한다.
+FOR /R %1 %%X IN (*.mp4 *.mkv) DO (
+ IF EXIST "%%~pX%%~nX.srt" (ffmpeg -i "%%X" -f srt -i "%%~pX%%~nX.srt" -map 0:0 -map 0:1 -map 1:0 -c:v copy -c:a copy -c:s srt -metadata:s:s:0 language=kor "%%~pX%%~nX_subs%%~xX")
+ IF EXIST "%%~pX%%~nX.ko.srt" (ffmpeg -i "%%X" -f srt -i "%%~pX%%~nX.kor.srt" -map 0:0 -map 0:1 -map 1:0 -c:v copy -c:a copy -c:s srt -metadata:s:s:0 language=kor "%%~pX%%~nX_subs%%~xX")
+ IF EXIST "%%~pX%%~nX.kor.srt" (ffmpeg -i "%%X" -f srt -i "%%~pX%%~nX.kor.srt" -map 0:0 -map 0:1 -map 1:0 -c:v copy -c:a copy -c:s srt -metadata:s:s:0 language=kor "%%~pX%%~nX_subs%%~xX")
+ IF EXIST "%%~pX%%~nX.ko.kor.srt" (ffmpeg -i "%%X" -f srt -i "%%~pX%%~nX.ko.kor.srt" -map 0:0 -map 0:1 -map 1:0 -c:v copy -c:a copy -c:s srt -metadata:s:s:0 language=kor "%%~pX%%~nX_subs%%~xX")
+ echo .
+)
+
+
+
+
+GOTO END
+
+
+:TWOFILE
+echo 파일 2개 선택됨 (아직 구현 안했어용)
+GOTO END
+
+:THREEFILE
+echo 파일 3개 선택됨 (아직 구현 안했어용)
+GOTO END
+
+
+
+%~xI
+:END
+pause
+exit
+이 아래는 제작자의 메모장입니다
+ffmpeg -i input.mp4 -f srt -i input.srt \
+ -map 0:0 -map 0:1 -map 1:0 -c:v copy -c:a copy \
+ -c:s srt  output.mkv
+ ffmpeg -i "input"\
+ -f srt -i "srt"\
+ -map 0:0 -map 0:1 -map 1:0 -c:v copy -c:a copy -c:s srt\
+ -metadata:s:s:0 language=kor output.mkv
+
+
+해결책
+특정 변수 수정자를 사용해야합니다. 다음은 작동하는 예입니다.
+
+if "%~x1" == ".ext" (echo File extension matches.)
+사용 가능한 수정자
+%~I         - expands %I removing any surrounding quotes (")
+%~fI        - expands %I to a fully qualified path name
+%~dI        - expands %I to a drive letter only
+%~pI        - expands %I to a path only
+%~nI        - expands %I to a file name only
+%~xI        - expands %I to a file extension only
+%~sI        - expanded path contains short names only
+%~aI        - expands %I to file attributes of file
+%~tI        - expands %I to date/time of file
+%~zI        - expands %I to size of file
+%~$PATH:I   - searches the directories listed in the PATH
+               environment variable and expands %I to the
+               fully qualified name of the first one found.
+               If the environment variable name is not
+               defined or the file is not found by the
+               search, then this modifier expands to the
+               empty string
+
+
+               FOR /R [경로] %%변수 IN (집합) DO 명령어
