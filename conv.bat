e91ff2836b327ec01afa7a1c46795f85050a6112
Kamilake <exjang0@gmail.com>
2021-04-16 21:14:59 +0900

    자막 인코딩

--- conv.bat
+++ conv.bat
@@ -11,9 +11,8 @@ SET Kamiconv_Overwrite=n
 @REM .smi 자막을 발견하면 자동으로 .srt로 변환 (y/n)
 SET Kamiconv_SAMI_to_SubRip=y
 
-@REM 자막 인코딩 (EUC-KR, UTF-8 etc...)
-SET Kamiconv_Encoding=UTF-8
-
+@REM 자막 인코딩 (cp949, EUC-KR, UTF-8 etc...)
+SET Kamiconv_Encoding=UTF-8S
 
 
 
@@ -51,7 +50,7 @@ GOTO END
 IF EXIST %1\* GOTO ONEFILE_ONEDIR
 echo 파일 1개 선택됨
 IF "%Kamiconv_SAMI_to_SubRip%" == "n" GOTO ONEFILE_ENDSMITOSRT
-IF EXIST "%~p1%~n1.smi" (ffmpeg -i "%~p1%~n1.smi" "%~p1%~n1.srt" -sub_charenc %Kamiconv_Encoding% -%Kamiconv_Overwrite%)
+IF EXIST "%~p1%~n1.smi" (ffmpeg -sub_charenc %Kamiconv_Encoding% -i "%~p1%~n1.smi" "%~p1%~n1.srt" -%Kamiconv_Overwrite%)
 :ONEFILE_ENDSMITOSRT
 
 
@@ -72,7 +71,7 @@ echo 폴더 1개 선택됨
 @REM 출력파일명 "%%~pX%%~nX_subs%%~xX"
 
 IF "%Kamiconv_SAMI_to_SubRip%" == "n" GOTO ONEFILE_ONEDIR_ENDSMITOSRT
-FOR /R %1 %%X IN (*.smi) DO (ffmpeg -i "%%X" "%%~pX%%~nX.srt" -sub_charenc %Kamiconv_Encoding% -%Kamiconv_Overwrite%)
+FOR /R %1 %%X IN (*.smi) DO (ffmpeg -sub_charenc %Kamiconv_Encoding% -i "%%X" "%%~pX%%~nX.srt" -%Kamiconv_Overwrite%)
 :ONEFILE_ONEDIR_ENDSMITOSRT
 
 
