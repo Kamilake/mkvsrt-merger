f71e4ff92a378b80c90216722a8102981b7886fa
Kamilake <exjang0@gmail.com>
2021-05-15 13:09:25 +0900

    자막 제거+두번째 오디오 스트림 기본값으로
    자막을 제거하고 두번째 오디오 스트림을 기본값으로 설정하는 배치파일

---
+++ rmsub+change default tracks.bat
@@ -0,0 +1,5 @@
+﻿@REM Encoding: UTF-8.. 자막 제거용 도구입니다.
+chcp 65001
+ffmpeg -i "%~1" -c copy -map 0:0 -map 0:1 "%~p1%~n1.copy%~x1"
+FOR /R %1 %%X IN (*.mkv *.mp4) DO (ffmpeg -i "%%X" -c copy -map 0:0 -map 0:2 -map 0:1 -disposition:a:0 default -disposition:a:1 none "%%~pX%%~nX.copy%%~xX")
+pause
