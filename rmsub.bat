489ba957a32bb1173d817fb685a26f5ca4fc48ea
Pulteki <ttojunw@gmail.com>
2022-11-21 17:34:24 +0900

    Delete rmsub.bat

--- rmsub.bat
+++
@@ -1,4 +0,0 @@
-@REM Encoding: EUC-KR .. 자막 제거용 도구입니다.
-ffmpeg -i "%~1" -c copy -map 0 "%~p1%~n1.copy%~x1"
-FOR /R %1 %%X IN (*.mkv *.mp4) DO (ffmpeg -i "%%X" -c copy -map 0 "%%~pX%%~nX.copy%%~xX")
-pause
